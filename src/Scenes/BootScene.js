e5f1a67d25e394d1d806445ea619cfaa4219d243
Nasa28 <kaluchinasaagu@gmail.com>
2021-09-13 12:27:52 +0100

    add BootScene class

---
+++ src/Scenes/BootScene.js
@@ -0,0 +1,12 @@
+
+import Phaser from 'phaser';
+
+export default class BootScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'BootScene' });
+  }
+
+  create() {
+    this.scene.start('PreloaderScene');
+  }
+}
