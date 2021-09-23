50f43728b7b160df6409667a48068826808cb8ec
Nasa28 <kaluchinasaagu@gmail.com>
2021-09-23 22:57:40 +0100

    Add test for chaserShip class

--- tests/CarrierShip.test.js
+++ tests/CarrierShip.test.js
@@ -1,18 +1,11 @@
 import CarrierShip from '../src/Entities/CarrierShip'
 import Entity from '../src/Entities/Entity'
 
-// jest.mock("../src/Entities/Player")
+describe('CarrierShip should be a subclass of Entity', () => {
+  it('CarrierShip is subclass of entity ', () => {
+    expect(CarrierShip).toBeSubclassOf(Entity);
+  });
 
-// describe('Returns a Json Object from API', () => {
-  
-//   it('The Data is an object', () => { 
-//     const spy = jest.spyOn(Player, 'moveUp');
-//     Player.moveUp();
-//     expect(spy).toHaveBeenCalled();
-// })
-// })
+})
 
 
-test('Enemy Laser should be a subclass of Entity', () => {
-  expect(CarrierShip).toBeSubclassOf(Entity);
-});
