c87f8fb13fc71a474b65b1f7a23f462ffa112b20
Nasa28 <kaluchinasaagu@gmail.com>
2021-09-23 23:00:11 +0100

    Add test for EnemyLaser class

---
+++ tests/EnemyLaser.test.js
@@ -0,0 +1,9 @@
+import EnemyLaser from '../src/Entities/EnemyLaser';
+import Entity from '../src/Entities/Entity'
+
+describe('EnemyLaser should be a subclass of Entity', () => {
+  it('EnemyLaser is subclass of entity ', () => {
+    expect(EnemyLaser).toBeSubclassOf(Entity);
+  });
+
+})
